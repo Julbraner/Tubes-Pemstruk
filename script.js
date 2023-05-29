bb29ba74dd4cc32d3ce3f4b5109b3b674f3baaad
Daniel Jull <127308898+Julbraner@users.noreply.github.com>
2023-05-29 20:27:19 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+// DARKMODE
+const darkButton = document.querySelector('.darkmode-button')
+
+darkButton.addEventListener('click', ()=>{
+  document.body.classList.toggle('darkmode')
+  if(darkButton.innerText == 'Darkmode'){
+    darkButton.innerText = 'Lightmode'
+  }
+  else{
+    darkButton.innerText = 'Darkmode'
+  }
+})
+
+// HAMBURGER 
+const ham = document.querySelector('.hamburger')
+const navMenu = document.querySelector('.navbar')
+
+ham.addEventListener('click', ()=>{
+    navMenu.classList.toggle("active")
+})
+
+const items = document.querySelectorAll('.nav-item')
+
+items.forEach(item =>{
+  item.addEventListener('click', ()=>{
+    navMenu.classList.remove("active")
+})
+})
+
+// SCROOL UP
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+  b;
+}
+window.addEventListener("scroll", scrollUp);
+
+// POP Up
+const readMoreLink = document.getElementById("readMoreLink");
+readMoreLink.addEventListener("click", function (event) {
+  event.preventDefault(); // Mencegah perilaku default dari elemen <a>
+  alert("Ini adalah pop-up!");
+});
